b567b55bd41b2ca6cc2c38d6553939be65fcd4e0
nami <cigarbox.nami@gmail.com>
2021-05-30 22:42:04 +0900

    added juggling with rensho to exhibition-events.csv

--- src/components/SectionFeaturedWork.vue
+++ src/components/SectionFeaturedWork.vue
@@ -1,6 +1,10 @@
 <template>
   <BaseSection title="Featured Work">
-    <iframe class="embedded" width="560" height="315" src="https://www.youtube.com/embed/OuAM-EueCBM" title="YouTube video player"
+    <iframe class="embedded" width="560" height="315" src="https://www.youtube.com/embed/OuAM-EueCBM"
+        title="YouTube video player" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/yDQJPHuUc6Q" title="YouTube video player"
         frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen></iframe>
   </BaseSection>
