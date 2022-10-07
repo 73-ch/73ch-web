b3770f26c33e8d6428d06e342776b6e840b5d07a
nami <cigarbox.nami@gmail.com>
2022-10-07 17:16:11 +0900

    build(2022/10/07)

--- src/components/ElementPublicationList.vue
+++ src/components/ElementPublicationList.vue
@@ -22,9 +22,13 @@
           href="https://digitalnature.slis.tsukuba.ac.jp/2021/07/terafoils-publication/">Project Page</a>
     </p>
     <p>
-      Nozomu Yoshida, <u>Kosaku Namikawa</u>, Yusuke Koroyasu, Yoshiki Nagatani, and YoichiOchiai. 2021.  Auditory-Centered Vocal Feedback System Using Solmization forTraining Absolute Pitch Without GUI. InHuman-Computer Interaction – INTERACT2021, Carmelo Ardito, Rosa Lanzilotti, Alessio Malizia, Helen Petrie, Antonio Pic-cinno, Giuseppe Desolda, and Kori Inkpen (Eds.). Springer International Publishing,Cham, 3–19.
-      <a class="project-link"
-          href="https://digitalnature.slis.tsukuba.ac.jp/2021/08/vocal_feedback/">Project Page</a>
+      Nozomu Yoshida,
+      <u>Kosaku Namikawa</u>, Yusuke Koroyasu, Yoshiki Nagatani, and YoichiOchiai. 2021. Auditory-Centered Vocal Feedback System Using Solmization forTraining Absolute Pitch Without GUI. InHuman-Computer Interaction – INTERACT2021, Carmelo Ardito, Rosa Lanzilotti, Alessio Malizia, Helen Petrie, Antonio Pic-cinno, Giuseppe Desolda, and Kori Inkpen (Eds.). Springer International Publishing,Cham, 3–19.
+      <a class="project-link" href="https://digitalnature.slis.tsukuba.ac.jp/2021/08/vocal_feedback/">Project Page</a>
+    </p>
+    <p>
+      Masaaki Sadasue, Ippei Suzuki, <u>Kosaku Namikawa</u>, Kengo Tanaka, Chieko Nishimura, Masaki Okamura, and Yoichi Ochiai. (2022). Development and Evaluation of Systems to Enjoy a Wedding Reception for People with Low Vision. In: Stephanidis, C., Antona, M., Ntoa, S. (eds) HCI International 2022 Posters. HCII 2022. Communications in Computer and Information Science, vol 1580. Springer, Cham.
+      <a href="https://doi.org/10.1007/978-3-031-06417-3_79">https://doi.org/10.1007/978-3-031-06417-3_79</a>
     </p>
   </div>
 </template>
