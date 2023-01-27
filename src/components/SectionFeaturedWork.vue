3c79b90cd559d75d6db0689420c9778ab8032100
nami <cigarbox.nami@gmail.com>
2023-01-28 00:45:37 +0900

    build(2023/01/28)

--- src/components/SectionFeaturedWork.vue
+++ src/components/SectionFeaturedWork.vue
@@ -4,12 +4,12 @@
       <element-featured-work title="Kosaku Namikawa / scrolling (2022)" url="https://scrolling.73ch.work/"
           image="thumbnails/thumbnail_scrolling.png"/>
       <element-featured-work title="Kosaku Namikawa & Yuta Okuyama / trajectory (2022)"
-          url="https://confusion-sense-3df.notion.site/trajectory-6e2c0a04ab4d4e68b9b1276468053814  " image="thumbnails/thumbnail_trajectory.png"/>
-      <element-featured-work title="Beyond Internet (2022)" url="https://confusion-sense-3df.notion.site/2021-Beyond-Internet-42c02cb24de6490396bdadc9d628a1d7"
+          url="https://73ch.notion.site/trajectory-6e2c0a04ab4d4e68b9b1276468053814  " image="thumbnails/thumbnail_trajectory.png"/>
+      <element-featured-work title="Beyond Internet (2022)" url="https://73ch.notion.site/2021-Beyond-Internet-42c02cb24de6490396bdadc9d628a1d7"
           image="thumbnails/thumbnail_digicon.png"/>
       <element-featured-work title="TParty / シブキ (2021)" url="https://www.youtube.com/watch?v=tRubYTfBBGc"
           image="thumbnails/thumbnail_shibuki.png"/>
-      <element-featured-work title="Kosaku Namikawa / presence (2021)" url="https://confusion-sense-3df.notion.site/ENDLESS-IMAGINARY-bef67a2477274bc687c0e98f069b2cfd"
+      <element-featured-work title="Kosaku Namikawa / presence (2021)" url="https://73ch.notion.site/ENDLESS-IMAGINARY-bef67a2477274bc687c0e98f069b2cfd"
           image="thumbnails/thumbnail_endless.png"/>
       <element-featured-work title="Kosaku Namikawa & Rensho Takahashi / Juggling Movie (2021)"
           url="https://www.youtube.com/watch?v=yDQJPHuUc6Q" image="thumbnails/thumbnail_juggling.png"/>
